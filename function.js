a4a074a010c0de7e59f0004f3411d2f38d35db6d
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-08-30 12:33:46 +0700

    add width setter button

---
+++ function.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+function increaseWidth() {
+  const root = document.querySelector(":root");
+  const cardWidth = parseInt(getComputedStyle(root).getPropertyValue("--card-width").slice().replace("px", ""));
+  root.style.setProperty("--card-width", cardWidth + 30 + 'px')
+}
+
+function decreaseWidth() {
+  const root = document.querySelector(":root");
+  const cardWidth = parseInt(getComputedStyle(root).getPropertyValue("--card-width").slice().replace("px", ""));
+  root.style.setProperty("--card-width", cardWidth - 30 + 'px')
+}
